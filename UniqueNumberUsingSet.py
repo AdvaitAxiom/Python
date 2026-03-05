4aba8d563f186e1848c908d82c94b854c5b422f5
Alapan000 <alapandas2810@gmail.com>
2026-03-05 13:15:07 +0530

    Unique number using set entered by user

---
+++ UniqueNumberUsingSet.py
@@ -0,0 +1,6 @@
+store = set()  # Create an empty set to store unique numbers
+for i in range(7):
+    num = int(input("Enter a number:"))
+    store.add(num)
+
+print("Unique numbers entered:", store)
